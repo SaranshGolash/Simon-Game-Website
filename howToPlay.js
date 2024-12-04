088ef101ab9cd8c40ef53d5672dffe8d903a5532
Saransh Golash <129923993+SaranshGolash@users.noreply.github.com>
2024-12-05 01:26:40 +0530

    Add files via upload

---
+++ howToPlay.js
@@ -0,0 +1,3 @@
+$("button").on("click", function() {
+    window.location.replace("index.html");
+});
---
+++ index.js
@@ -0,0 +1,7 @@
+$(".btn-success").on("click", function() {
+    window.location.replace("play.html");
+});
+
+$(".btn-info").on("click", function() {
+    window.location.replace("howToPlay.html");
+});
